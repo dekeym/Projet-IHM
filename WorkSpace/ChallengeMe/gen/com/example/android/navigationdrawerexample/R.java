69e3d421f2785e23d5b962332eb51a5345d748e7
dekey <stephane.sb@live.fr>
2014-12-17 13:20:39 +0100

    dev ihm page accueil OK

--- WorkSpace/ChallengeMe/gen/com/example/android/navigationdrawerexample/R.java
+++ WorkSpace/ChallengeMe/gen/com/example/android/navigationdrawerexample/R.java
@@ -9,7 +9,7 @@ package com.example.android.navigationdrawerexample;
 
 public final class R {
     public static final class array {
-        public static final int planets_array=0x7f050000;
+        public static final int menu=0x7f050000;
     }
     public static final class attr {
     }
@@ -29,31 +29,37 @@ public final class R {
         public static final int venus=0x7f02000c;
     }
     public static final class id {
-        public static final int action_websearch=0x7f07000a;
-        public static final int activity=0x7f070003;
-        public static final int activity1=0x7f070006;
-        public static final int activity2=0x7f070007;
         public static final int content_frame=0x7f070001;
+        public static final int date=0x7f07000b;
+        public static final int distance=0x7f07000d;
         public static final int drawer_layout=0x7f070000;
-        public static final int history=0x7f070005;
+        public static final int dst=0x7f07000e;
+        public static final int history=0x7f070004;
+        public static final int imageView1=0x7f07000a;
         public static final int left_drawer=0x7f070002;
-        public static final int listActivity1=0x7f070009;
-        public static final int listActivity2=0x7f070008;
-        public static final int logoChallengeMe=0x7f070004;
+        public static final int listTeamHistory=0x7f070009;
+        public static final int listUserHistory=0x7f070008;
+        public static final int logoChallengeMe=0x7f070003;
+        public static final int mainTitle=0x7f070005;
+        public static final int teamHistoryTitle=0x7f070007;
+        public static final int time=0x7f07000c;
+        public static final int timefixed=0x7f070010;
+        public static final int timevar=0x7f07000f;
+        public static final int userHistoryTitle=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int drawer_list_item=0x7f030001;
-        public static final int fragment_planet=0x7f030002;
+        public static final int fragment_accueil=0x7f030002;
         public static final int fragment_planeted=0x7f030003;
+        public static final int rowlayout=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f060000;
     }
     public static final class string {
-        public static final int action_websearch=0x7f040003;
         public static final int app_name=0x7f040000;
-        public static final int app_not_available=0x7f040004;
+        public static final int app_not_available=0x7f040003;
         public static final int drawer_close=0x7f040002;
         public static final int drawer_open=0x7f040001;
     }
--- WorkSpace/ChallengeMe/src/com/example/android/challengeme/MainActivity.java
+++ WorkSpace/ChallengeMe/src/com/example/android/challengeme/MainActivity.java
@@ -1,30 +1,16 @@
-/*
- * Copyright 2013 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.challengeme;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.example.android.challengemestructure.Course;
+import com.example.android.challengemestructure.ListCourses;
 import com.example.android.navigationdrawerexample.R;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Intent;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -38,37 +24,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-/**
- * This example illustrates a common usage of the DrawerLayout widget
- * in the Android support library.
- * <p/>
- * <p>When a navigation (left) drawer is present, the host activity should detect presses of
- * the action bar's Up affordance as a signal to open and close the navigation drawer. The
- * ActionBarDrawerToggle facilitates this behavior.
- * Items within the drawer should fall into one of two categories:</p>
- * <p/>
- * <ul>
- * <li><strong>View switches</strong>. A view switch follows the same basic policies as
- * list or tab navigation in that a view switch does not create navigation history.
- * This pattern should only be used at the root activity of a task, leaving some form
- * of Up navigation active for activities further down the navigation hierarchy.</li>
- * <li><strong>Selective Up</strong>. The drawer allows the user to choose an alternate
- * parent for Up navigation. This allows a user to jump across an app's navigation
- * hierarchy at will. The application should treat this as it treats Up navigation from
- * a different task, replacing the current task stack using TaskStackBuilder or similar.
- * This is the only form of navigation drawer that should be used outside of the root
- * activity of a task.</li>
- * </ul>
- * <p/>
- * <p>Right side drawers should be used for actions, not navigation. This follows the pattern
- * established by the Action Bar that navigation should be to the left and actions to the right.
- * An action should be an operation performed on the current contents of the window,
- * for example enabling or disabling a data overlay on top of the current content.</p>
- */
 public class MainActivity extends Activity {
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -76,26 +36,29 @@ public class MainActivity extends Activity {
 
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-    private String[] mPlanetTitles;
-
+    private String[] menuTitles;
+    
+    private MyListAdapter adapterList;
+    private ListCourses historique = new ListCourses();   
+      
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+                      
         mTitle = mDrawerTitle = getTitle();
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        menuTitles = getResources().getStringArray(R.array.menu);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // set a custom shadow that overlays the main content when the drawer opens
+        // mettre une ombre qui recouvre le contenu principal quand on ouvre le menu
         mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // set up the drawer's list view with items and click listener
+        // mise en place de liste du menu et création du listener sur les items de la liste
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
+                R.layout.drawer_list_item, menuTitles));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        // enable ActionBar app icon to behave as action to toggle nav drawer
+        // autoriser l'action d'ouvrir/fermer le menu lorsqu'on clique sur l'icône de l'app
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
 
@@ -123,6 +86,7 @@ public class MainActivity extends Activity {
         if (savedInstanceState == null) {
             selectItem(0);
         }
+        
     }
 
     @Override
@@ -137,7 +101,7 @@ public class MainActivity extends Activity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         // If the nav drawer is open, hide action items related to the content view
         boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -150,17 +114,7 @@ public class MainActivity extends Activity {
         }
         // Handle action buttons
         switch(item.getItemId()) {
-        case R.id.action_websearch:
-            // create intent to perform web search for this planet
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-            // catch event that there's no activity to handle intent
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            } else {
-                Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
-            }
-            return true;
+        
         default:
             return super.onOptionsItemSelected(item);
         }
@@ -175,6 +129,8 @@ public class MainActivity extends Activity {
     }
 
     private void selectItem(int position) {
+        adapterList = new MyListAdapter(this, R.layout.rowlayout, historique.getListeCourses());
+    	
         // update the main content by replacing fragments
         Fragment fragment = new PlanetFragment();
         Bundle args = new Bundle();
@@ -187,19 +143,21 @@ public class MainActivity extends Activity {
 
             // update selected item and title, then close the drawer
             mDrawerList.setItemChecked(position, true);
-            setTitle(mPlanetTitles[position]);
+            setTitle(menuTitles[position]);
             mDrawerLayout.closeDrawer(mDrawerList);
+       
         }else{
-        	Fragment fragmented = new PlanetFragmented();
+ 
+            Fragment fragmented = new PlanetFragmented();
         	args.putInt(PlanetFragmented.ARG_PLANET_NUMBER, position);
         	fragmented.setArguments(args);
 
             FragmentManager fragmentManager = getFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.content_frame, fragmented).commit();
-
+            
             // update selected item and title, then close the drawer
             mDrawerList.setItemChecked(position, true);
-            setTitle(mPlanetTitles[position]);
+            setTitle(menuTitles[position]);
             mDrawerLayout.closeDrawer(mDrawerList);
         }
         
@@ -233,7 +191,7 @@ public class MainActivity extends Activity {
     /**
      * Fragment that appears in the "content_frame", shows a planet
      */
-    public static class PlanetFragment extends Fragment {
+    public class PlanetFragment extends Fragment {
         public static final String ARG_PLANET_NUMBER = "planet_number";
 
         public PlanetFragment() {
@@ -243,11 +201,18 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_accueil, container, false);
             int i = getArguments().getInt(ARG_PLANET_NUMBER);
-            String planet = getResources().getStringArray(R.array.planets_array)[i];
-
-
+            String planet = getResources().getStringArray(R.array.menu)[i];
+         
+            // récupération de la liste teamActivity sur le layout
+            ListView teamActivity = (ListView) rootView.findViewById(R.id.listTeamHistory);
+            teamActivity.setAdapter(adapterList);
+            
+            // récupération de la liste teamActivity sur le layout
+            ListView userActivity = (ListView) rootView.findViewById(R.id.listUserHistory);
+            userActivity.setAdapter(adapterList);
+           
             return rootView;
         }
     }
@@ -264,10 +229,12 @@ public class MainActivity extends Activity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_planeted, container, false);
             int i = getArguments().getInt(ARG_PLANET_NUMBER);
-            String planet = getResources().getStringArray(R.array.planets_array)[i];
+            String planet = getResources().getStringArray(R.array.menu)[i];
 
 
             return rootView;
         }
+        
     }
+    
 }
